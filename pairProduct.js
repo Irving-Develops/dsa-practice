1dc5bb49f7f253cf88dad3b8c907cb60cbe67e55
Irving Arreola Palacios <irving.a789@gmail.com>
2022-10-29 20:17:24 -0500

    my solution to pairProduct

---
+++ pairProduct.js
@@ -0,0 +1,27 @@
+/* 
+Write a function, pairProduct, that takes in an array and a target product as arguments.The
+function should
+return an array containing a pair of indices whose elements multiply to the given target.The indices returned must be unique.
+
+Be sure to
+return the indices, not the elements themselves.
+
+There is guaranteed to be one such pair whose product is the target.
+*/
+
+const pairProduct = (numbers, targetProduct) => {
+    // todo
+
+    let productPair = {}
+
+    for (let i = 0; i < numbers.length; i++) {
+        let num = numbers[i]
+        let otherNum = targetProduct / num
+
+        if (otherNum in productPair) return [i, productPair[otherNum]]
+        productPair[num] = i
+
+
+    }
+};
+
--- pairSum.js
+++ pairSum.js
@@ -10,20 +10,35 @@ There is guaranteed to be one such pair that sums to the target.
 
 */
 
-const pairSum = (numbers, targetSum) => {
-    // todo
-    const sumPair = []
-    for (let i = 0; i < numbers.length; i++) {
-        let num1 = numbers[i]
-        for (let j = i + 1; j < numbers.length; j++) {
-            let num2 = numbers[j]
-            console.log(num1, num2)
-            if (num1 + num2 === targetSum) {
-                sumPair.push(i, j)
-            }
-        }
-    }
-    if (sumPair.length === 0) return -1
-    return sumPair
+// const pairSum = (numbers, targetSum) => {
+//     // todo
+//     const sumPair = []
+//     for (let i = 0; i < numbers.length; i++) {
+//         let num1 = numbers[i]
+//         for (let j = i + 1; j < numbers.length; j++) {
+//             let num2 = numbers[j]
+//             console.log(num1, num2)
+//             if (num1 + num2 === targetSum) {
+//                 sumPair.push(i, j)
+//             }
+//         }
+//     }
+//     if (sumPair.length === 0) return -1
+//     return sumPair
 
+// };
+
+const pairSum = (numbers, targetSum) => {
+    const previousNums = {};
+    for (let i = 0; i < numbers.length; i += 1) {
+        const num = numbers[i];
+        const complement = targetSum - num;
+        if (complement in previousNums) 
+        // console.log(complement, previousNums)
+        return [i, previousNums[complement]];
+
+        previousNums[num] = i;
+    }
 };
+
+console.log(pairSum([3, 2, 5, 4, 1], 8))
